b0d62abd7d3d6d6a3f1e451f0a93e701c706ecd8
M. Iqbal Effendi <iqbaleff214@gmail.com>
2025-02-05 14:14:35 +0800

    initial user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'M. Iqbal Effendi',
+            'email' => 'iqbaleff214@gmail.com',
         ]);
     }
 }
